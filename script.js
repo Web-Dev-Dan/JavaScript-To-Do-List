2dd26cc5b88f33a6ceb62fc645797e1a4f9e8aab
Dan <80458000+Web-Dev-Dan@users.noreply.github.com>
2021-12-11 22:57:34 +0900

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,11 @@ const year = newDate.getFullYear();
 
 const todaysFullDate = `${weekdays[day]} ${date} ${months[month]}, ${year}`;
 
+function printDate() {
+    return `Added ${weekdays[day]} ${date} ${months[month]}, ${year}`;
+}
+
+
 
 // ---------- Toggle Light/Dark --------------
 const sunIcon = document.getElementById('sunIcon');
@@ -60,11 +65,19 @@ function toggleModal() {
 
 
 
+// ------- Toggle To-Do Container Order Button --------
+const toggleListOrderBtn = document.getElementById('toggleListOrderBtn');
+const toDoItemContainer = document.querySelector('.to-do-item-container');
+
+toggleListOrderBtn.addEventListener('click', () => {
+    toDoItemContainer.classList.toggle('reverse-order');
+});
+
 
 // ---------- To-Do List --------------
 let listName = 'To-Do List';
-let userName = 'Daniel';
-let listedTodos = 1;
+let userName = 'User';
+let listedTodos = 0;
 let completedTodos = 0;
 let prioritisedTodos = 0;
 
@@ -73,30 +86,161 @@ const userNameText = document.querySelectorAll('#userNameText');
 const listedTodosText = document.querySelectorAll('#listedTodosText');
 const prioritisedText = document.querySelectorAll('#prioritisedText');
 
-listNameText.forEach((list) => {
-    list.textContent = listName;
-});
+function updateUserInfo() {
+    listNameText.forEach((list) => {
+        list.textContent = listName;
+    });
 
-userNameText.forEach((name) => {
-    name.textContent = userName;
-});
+    userNameText.forEach((name) => {
+        name.textContent = userName;
+    });
+}
 
-listedTodosText.forEach((todos) => {
-    const listedTodosInlineText = document.getElementById('listedTodosInlineText');
+function updateSummary() {
+    listedTodosText.forEach((todos) => {
+        const listedTodosInlineText = document.getElementById('listedTodosInlineText');
 
-    if (listedTodos === 1) {
-        listedTodosInlineText.textContent = 'is';
-        todos.textContent = `${listedTodos} item`;
+        if (listedTodos === 1) {
+            listedTodosInlineText.textContent = 'is';
+            todos.textContent = `${listedTodos} item`;
+        } else {
+            listedTodosInlineText.textContent = 'are';
+            todos.textContent = `${listedTodos} items`;
+        }
+    });
+
+    prioritisedText.forEach((priority) => {
+        if (prioritisedTodos === 1) {
+            priority.textContent = `${prioritisedTodos} prioritised item`;
+        } else {
+            priority.textContent = `${prioritisedTodos} prioritised items`;
+        }
+    });
+}
+
+function checkListedToDos() {
+    const itemContainer = document.querySelector('.item-container');
+    const emptyItemContainer = document.querySelector('.empty-list-display');
+
+    if (listedTodos !== 0) {
+        itemContainer.style.display = 'flex';
+        emptyItemContainer.style.display = 'none';
     } else {
-        listedTodosInlineText.textContent = 'are';
-        todos.textContent = `${listedTodos} items`;
+        itemContainer.style.display = 'none';
+        emptyItemContainer.style.display = 'flex';
+    }
+}
+
+
+// ---------- ⭐️ Add a To-Do ⭐️ --------------
+const addItemBtn = document.querySelectorAll('#addItemBtn');
+const addItemInput = document.querySelectorAll('#addItemInput');
+let inputFieldActive = false;
+
+addItemBtn.forEach((addBtn) => {
+    addBtn.addEventListener('click', addItemToList);
+});
+
+//--Add Item to List on 'Enter'--
+document.addEventListener('keydown', function () {
+    addItemInput.forEach((input) => {
+        if (input === document.activeElement) {
+            inputFieldActive = true;
+        } else {
+            inputFieldActive = false;
+        }
+    });
+});
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        addItemToList();
     }
 });
+//-----
 
-prioritisedText.forEach((priority) => {
-    if (prioritisedTodos === 1) {
-        priority.textContent = `${prioritisedTodos} prioritised item`;
-    } else {
-        priority.textContent = `${prioritisedTodos} prioritised items`;
-    }
-});
+function addItemToList() {
+    addItemInput.forEach((inputValue) => {
+        if (inputValue.value !== '') {
+            listedTodos++;
+            updateSummary();
+            addNewItemToList();
+            checkListedToDos();
+            inputValue.value = '';
+        }
+    });
+}
+
+function addNewItemToList() {
+    createNewItemElement();
+}
+
+// **Create the To-Do Item
+function createNewItemElement() {
+    const addItemInput = document.getElementById('addItemInput');
+    const dateText = printDate();
+    const toDoContainer = document.querySelector('.to-do-item-container');
+
+    const newItemContainer = document.createElement('div');
+    newItemContainer.classList.add('to-do-item');
+    toDoContainer.appendChild(newItemContainer);
+
+    const newItemContentMain = document.createElement('div');
+    newItemContentMain.classList.add('item-content-main');
+    newItemContainer.appendChild(newItemContentMain);
+
+    const newToDoItemCheckbox = document.createElement('div');
+    newToDoItemCheckbox.classList.add('to-do-item__checkbox');
+    newItemContentMain.appendChild(newToDoItemCheckbox);
+
+    const newCircleIcon = document.createElement('i');
+    newCircleIcon.classList.add('far', 'fa-circle', 'fa-2x');
+    newToDoItemCheckbox.appendChild(newCircleIcon);
+
+    const newToDoItemContent = document.createElement('div');
+    newToDoItemContent.classList.add('to-do-item__content');
+    newItemContentMain.appendChild(newToDoItemContent);
+
+    const newContentText = document.createElement('div');
+    newContentText.classList.add('content-text');
+    newToDoItemContent.appendChild(newContentText);
+
+    const newContentTextToDo = document.createElement('p');
+    newContentTextToDo.classList.add('content-text__to-do');
+    newContentTextToDo.textContent = addItemInput.value;
+    newContentText.appendChild(newContentTextToDo);
+
+    const newContentTextDate = document.createElement('p');
+    newContentTextDate.classList.add('content-text__date');
+    newContentTextDate.textContent = dateText;
+    newContentText.appendChild(newContentTextDate);
+
+    const newItemContentIcons = document.createElement('div');
+    newItemContentIcons.classList.add('item-content-icons');
+    newItemContainer.appendChild(newItemContentIcons);
+
+    const newToDoItemPriority = document.createElement('div');
+    newToDoItemPriority.classList.add('to-do-item__priority');
+    newItemContentIcons.appendChild(newToDoItemPriority);
+
+    const newStarIcon = document.createElement('i');
+    newStarIcon.classList.add('far', 'fa-star');
+    newToDoItemPriority.appendChild(newStarIcon);
+
+    const newToDoItemEdit = document.createElement('div');
+    newToDoItemEdit.classList.add('to-do-item__edit');
+    newItemContentIcons.appendChild(newToDoItemEdit);
+
+    const newEditIcon = document.createElement('i');
+    newEditIcon.classList.add('fas', 'fa-edit');
+    newToDoItemEdit.appendChild(newEditIcon);
+
+    const newToDoItemRemove = document.createElement('div');
+    newToDoItemRemove.classList.add('to-do-item__remove');
+    newItemContentIcons.appendChild(newToDoItemRemove);
+
+    const newTrashIcon = document.createElement('i');
+    newTrashIcon.classList.add('fas', 'fa-trash-alt');
+    newToDoItemRemove.appendChild(newTrashIcon);
+
+}
