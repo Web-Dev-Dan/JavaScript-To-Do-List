1bfac5c0d73ef17c5f856ab511f7f5e39c196a0d
Dan <80458000+Web-Dev-Dan@users.noreply.github.com>
2021-12-12 19:32:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ function printDate() {
 }
 
 
-
 // ---------- Toggle Light/Dark --------------
 const sunIcon = document.getElementById('sunIcon');
 const moonIcon = document.getElementById('moonIcon');
@@ -65,12 +64,78 @@ function toggleModal() {
 
 
 
-// ------- Toggle To-Do Container Order Button --------
-const toggleListOrderBtn = document.getElementById('toggleListOrderBtn');
+
+// ------- To-Do List Filters --------
+// 1. Toggle To-Do Container Order Button:
 const toDoItemContainer = document.querySelector('.to-do-item-container');
+const toggleListOrderBtn = document.getElementById('toggleListOrderBtn');
+const showAllBtn = document.getElementById('showAllBtn');
+const showCompleteBtn = document.getElementById('showCompleteBtn');
+const showIncompleteBtn = document.getElementById('showIncompleteBtn');
+const showPrioritisedBtn = document.getElementById('showPrioritisedBtn');
 
 toggleListOrderBtn.addEventListener('click', () => {
     toDoItemContainer.classList.toggle('reverse-order');
+});
+// 2. Toggle 'All'
+showAllBtn.addEventListener('click', () => {
+    showAllBtn.classList.add('filter-active');
+    showCompleteBtn.classList.remove('filter-active');
+    showIncompleteBtn.classList.remove('filter-active');
+    showPrioritisedBtn.classList.remove('filter-active');
+
+    const listItem = document.querySelectorAll('.to-do-item');
+    listItem.forEach((item) => {
+        item.style.display = 'flex';
+    });
+});
+// 3. Toggle 'Complete'
+showCompleteBtn.addEventListener('click', () => {
+    showAllBtn.classList.remove('filter-active');
+    showCompleteBtn.classList.add('filter-active');
+    showIncompleteBtn.classList.remove('filter-active');
+    showPrioritisedBtn.classList.remove('filter-active');
+
+    const listItem = document.querySelectorAll('.to-do-item');
+    listItem.forEach((item) => {
+        if (item.classList.contains('item-complete')) {
+            item.style.display = 'flex';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+});
+// 4. Toggle 'Incomplete'
+showIncompleteBtn.addEventListener('click', () => {
+    showAllBtn.classList.remove('filter-active');
+    showCompleteBtn.classList.remove('filter-active');
+    showIncompleteBtn.classList.add('filter-active');
+    showPrioritisedBtn.classList.remove('filter-active');
+
+    const listItem = document.querySelectorAll('.to-do-item');
+    listItem.forEach((item) => {
+        if (item.classList.contains('item-complete')) {
+            item.style.display = 'none';
+        } else {
+            item.style.display = 'flex';
+        }
+    });
+});
+// 5. Toggle 'Prioritised'
+showPrioritisedBtn.addEventListener('click', () => {
+    showAllBtn.classList.remove('filter-active');
+    showCompleteBtn.classList.remove('filter-active');
+    showIncompleteBtn.classList.remove('filter-active');
+    showPrioritisedBtn.classList.add('filter-active');
+
+    const listItem = document.querySelectorAll('.to-do-item');
+    listItem.forEach((item) => {
+        if (item.classList.contains('priority-item')) {
+            item.style.display = 'flex';
+        } else {
+            item.style.display = 'none';
+        }
+    });
 });
 
 
@@ -171,9 +236,6 @@ function addItemToList() {
     });
 }
 
-function addNewItemToList() {
-    createNewItemElement();
-}
 
 // **Create the To-Do Item
 function createNewItemElement() {
@@ -224,7 +286,7 @@ function createNewItemElement() {
     newItemContentIcons.appendChild(newToDoItemPriority);
 
     const newStarIcon = document.createElement('i');
-    newStarIcon.classList.add('far', 'fa-star');
+    newStarIcon.classList.add('far', 'fa-star', 'star-icon');
     newToDoItemPriority.appendChild(newStarIcon);
 
     const newToDoItemEdit = document.createElement('div');
@@ -242,5 +304,88 @@ function createNewItemElement() {
     const newTrashIcon = document.createElement('i');
     newTrashIcon.classList.add('fas', 'fa-trash-alt');
     newToDoItemRemove.appendChild(newTrashIcon);
+
+}
+
+
+
+function addNewItemToList() {
+    createNewItemElement();
+
+    // ---------- ✅ Complete Checkbox ✅ --------------
+    const checkboxCircleIcon = document.querySelectorAll('.fa-circle');
+
+    checkboxCircleIcon.forEach((checkbox) => {
+        checkbox.addEventListener('click', checkClickedIcon);
+    });
+
+    // ---------- ⭐️ Prioritise Button ⭐️ --------------
+    const starIcon = document.querySelectorAll('.star-icon');
+
+    starIcon.forEach((star) => {
+        star.addEventListener('click', checkClickedIcon);
+    });
+}
+
+function checkClickedIcon(e) {
+    const icon = e.target;
+
+    const iconParent = icon.parentElement;
+    const iconGrandparent = iconParent.parentElement;
+    const iconItem = iconGrandparent.parentElement;
+
+    // If unchecked, change to checked:
+    if (icon.classList.contains('fa-circle')) {
+        if (iconItem.classList.contains('priority-item')) {
+            icon.classList.remove('far', 'fa-circle');
+            icon.classList.add('fas', 'fa-check-circle');
+
+            iconItem.classList.remove('priority-item');
+            iconItem.classList.add('item-complete');
+
+            console.log(iconItem);
+
+            listedTodos--;
+            prioritisedTodos--;
+            updateSummary();
+        } else {
+            icon.classList.remove('far', 'fa-circle');
+            icon.classList.add('fas', 'fa-check-circle');
+
+            iconItem.classList.add('item-complete');
+
+            console.log(iconItem);
+
+            listedTodos--;
+            updateSummary();
+        }
+
+    }
+
+    // If blank star, change to filled (prioritised) star:
+    else if (icon.classList.contains('far', 'fa-star')) {
+        if (iconItem.classList.contains('item-complete')) {
+            // Nothing happens
+        } else {
+            icon.classList.remove('far');
+            icon.classList.add('fas');
+            iconItem.classList.add('priority-item');
+            prioritisedTodos++;
+            updateSummary();
+        }
+    }
+
+    // If filled (prioritised) star, change to blank star:
+    else if (icon.classList.contains('fas', 'fa-star')) {
+        if (iconItem.classList.contains('item-complete')) {
+            // Nothing happens
+        } else {
+            icon.classList.remove('fas');
+            icon.classList.add('far');
+            iconItem.classList.remove('priority-item');
+            prioritisedTodos--;
+            updateSummary();
+        }
+    }
 
 }
